8f8842dfe93ad4d68c049647cb527cad477ad193
Alina Christenbury <alina.christenbury@gmail.com>
2019-01-20 12:43:59 -0500

    fixed key

---
+++ Assets/_scripts/KeyInteraction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyInteraction : MonoBehaviour
+{
+    public GameObject Timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "door")
+        {
+            Timer.GetComponent<Timer>().isWin = true;
+            Debug.Log("win");
+        }
+    }
+}
